f8d68ea1c182296c0e904516425dcaaabbe3154d
Kin Lane <kinlane@gmail.com>
2023-08-09 17:11:12 -0400

    debug

--- paths/search/apis/get/index.js
+++ paths/search/apis/get/index.js
@@ -38,7 +38,7 @@ exports.handler = vandium.generic()
        sql_search += " OR a.tags LIKE '%" + search + "%')";
     }
 
-    sql =+ sql_search;
+    sql = sql + sql_search;
     
     connection.query(sql, function (error, results1, fields) { 
       
