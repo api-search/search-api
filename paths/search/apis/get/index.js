4cf2556807984f8e0d477b3f0331dfafb51141ab
Kin Lane <kinlane@gmail.com>
2023-08-13 18:54:02 -0400

    update search

--- paths/search/apis/get/index.js
+++ paths/search/apis/get/index.js
@@ -45,7 +45,7 @@ exports.handler = vandium.generic()
       const api_count = results1[0].api_count;
       //const api_count = 0;
     
-      let sql2 = "SELECT a.name,a.description,a.image,a.baseURL,a.humanURL,a.apisjson_url,a.tags,GROUP_CONCAT(CONCAT(p.type,'~', p.url)) as properties FROM apis a LEFT JOIN properties p ON a.baseURL = p.api_base_url WHERE a.id IS NOT NULL" + sql_search + " GROUP BY a.name LIMIT " + page + "," + limit;
+      let sql2 = "SELECT a.name,a.description,a.image,a.baseURL,a.humanURL,a.apisjson_url,a.tags,(select score from apisjson aj WHERE aj.url = a.apisjson_url) as score,(select percentage from apisjson aj WHERE aj.url = a.apisjson_url) as percentage,GROUP_CONCAT(CONCAT(p.type,'~', p.url)) as properties FROM apis a LEFT JOIN properties p ON a.baseURL = p.api_base_url WHERE a.id IS NOT NULL" + sql_search + " GROUP BY a.name LIMIT " + page + "," + limit;
       
       connection.query(sql2, function (error, results2, fields) {
   
@@ -89,6 +89,8 @@ exports.handler = vandium.generic()
           
             let d = {};
             d.name = results2[i].name;
+            d.rating = results2[i].rating;
+            d.percentage = results2[i].percentage;
             d.description = results2[i].description;
             d.image = results2[i].image;
             d.baseURL = results2[i].baseURL;
